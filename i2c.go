33de0fa43b557c91f88e554c710c96d8f760c8db
Erik Unger <erik@erikunger.com>
2013-12-19 22:46:17 +0100

    removed dependency from libi2c-dev

--- i2c.go
+++ i2c.go
@@ -51,34 +51,45 @@ func (i2c *I2C) SetAddress(address int) error {
 	return nil
 }
 
-// Perform SMBus Read Byte transaction.
-func (i2c *I2C) ReadUint8() (uint8, error) {
-	result, err := C.i2c_smbus_read_byte(C.int(i2c.file.Fd()))
-	if result == -1 {
+func (i2c *I2C) SmbusAccess(readWrite, command uint8, size int, data unsafe.Pointer) (uintptr, error) {
+	args := C.struct_i2c_smbus_ioctl_data{
+		read_write: C.char(readWrite),
+		command:    C.__u8(command),
+		size:       C.int(size),
+		data:       (*C.union_i2c_smbus_data)(data),
+	}
+	result, _, err := syscall.Syscall(syscall.SYS_IOCTL, i2c.file.Fd(), C.I2C_SMBUS, uintptr(unsafe.Pointer(&args)))
+	if int(result) == -1 {
 		return 0, err
 	}
-	return uint8(result), nil
+	return result, nil
+}
+
+// Perform SMBus Quick transaction.
+func (i2c *I2C) WriteQuick(value uint8) error {
+	_, err := i2c.SmbusAccess(value, 0, C.I2C_SMBUS_QUICK, nil)
+	return err
+}
+
+// Perform SMBus Read Byte transaction.
+func (i2c *I2C) ReadUint8() (result uint8, err error) {
+	_, err = i2c.SmbusAccess(C.I2C_SMBUS_READ, 0, C.I2C_SMBUS_BYTE, unsafe.Pointer(&result))
+	if err != nil {
+		return 0, err
+	}
+	return 0xFF & result, nil
 }
 
 // Perform SMBus Write Byte transaction.
 func (i2c *I2C) WriteUint8(value uint8) error {
-	result, err := C.i2c_smbus_write_byte(C.int(i2c.file.Fd()), C.__u8(value))
-	if result == -1 {
-		return err
-	}
-	return nil
+	_, err := i2c.SmbusAccess(C.I2C_SMBUS_WRITE, value, C.I2C_SMBUS_BYTE, nil)
+	return err
 }
 
 // Perform SMBus Read Byte transaction.
 func (i2c *I2C) ReadInt8() (int8, error) {
-	result, err := C.i2c_smbus_read_byte(C.int(i2c.file.Fd()))
-	if result == -1 {
-		return 0, err
-	}
-	if result >= 1<<7 {
-		result -= 1 << 8
-	}
-	return int8(result), nil
+	result, err := i2c.ReadUint8()
+	return int8(result), err
 }
 
 // Perform SMBus Write Byte transaction.
@@ -87,33 +98,24 @@ func (i2c *I2C) WriteInt8(value int8) error {
 }
 
 // Perform SMBus Read Byte Data transaction.
-func (i2c *I2C) ReadUint8Cmd(command uint8) (uint8, error) {
-	result, err := C.i2c_smbus_read_byte_data(C.int(i2c.file.Fd()), C.__u8(command))
-	if result == -1 {
+func (i2c *I2C) ReadUint8Cmd(command uint8) (result uint8, err error) {
+	_, err = i2c.SmbusAccess(C.I2C_SMBUS_READ, command, C.I2C_SMBUS_BYTE_DATA, unsafe.Pointer(&result))
+	if err != nil {
 		return 0, err
 	}
-	return uint8(result), nil
+	return 0xFF & result, nil
 }
 
 // Perform SMBus Write Byte Data transaction.
 func (i2c *I2C) WriteUint8Cmd(command uint8, value uint8) error {
-	result, err := C.i2c_smbus_write_byte_data(C.int(i2c.file.Fd()), C.__u8(command), C.__u8(value))
-	if result == -1 {
-		return err
-	}
-	return nil
+	_, err := i2c.SmbusAccess(C.I2C_SMBUS_WRITE, command, C.I2C_SMBUS_BYTE_DATA, unsafe.Pointer(&value))
+	return err
 }
 
 // Perform SMBus Read Byte Data transaction.
 func (i2c *I2C) ReadInt8Cmd(command uint8) (int8, error) {
-	result, err := C.i2c_smbus_read_byte_data(C.int(i2c.file.Fd()), C.__u8(command))
-	if result == -1 {
-		return 0, err
-	}
-	if result >= 1<<7 {
-		result -= 1 << 8
-	}
-	return int8(result), nil
+	result, err := i2c.ReadUint8Cmd(command)
+	return int8(result), err
 }
 
 // Perform SMBus Write Byte Data transaction.
@@ -122,47 +124,29 @@ func (i2c *I2C) WriteInt8Cmd(command uint8, value int8) error {
 }
 
 // Perform SMBus Read Word Data transaction.
-func (i2c *I2C) ReadUint16Cmd(command uint8) (uint16, error) {
-	result, err := C.i2c_smbus_read_word_data(C.int(i2c.file.Fd()), C.__u8(command))
-	if result == -1 {
+func (i2c *I2C) ReadUint16Cmd(command uint8) (result uint16, err error) {
+	_, err = i2c.SmbusAccess(C.I2C_SMBUS_READ, command, C.I2C_SMBUS_WORD_DATA, unsafe.Pointer(&result))
+	if err != nil {
 		return 0, err
 	}
-	return uint16(result), nil
+	return 0xFFFF & result, nil
 }
 
 // Perform SMBus Write Word Data transaction.
 func (i2c *I2C) WriteUint16Cmd(command uint8, value uint16) error {
-	result, err := C.i2c_smbus_write_word_data(C.int(i2c.file.Fd()), C.__u8(command), C.__u16(value))
-	if result == -1 {
-		return err
-	}
-	return nil
+	_, err := i2c.SmbusAccess(C.I2C_SMBUS_WRITE, command, C.I2C_SMBUS_WORD_DATA, unsafe.Pointer(&value))
+	return err
 }
 
 // Perform SMBus Read Word Data transaction.
 func (i2c *I2C) ReadInt16Cmd(command uint8) (int16, error) {
-	result, err := C.i2c_smbus_read_word_data(C.int(i2c.file.Fd()), C.__u8(command))
-	if result == -1 {
-		return 0, err
-	}
-	if result >= 1<<15 {
-		result -= 1 << 16
-	}
-	return int16(result), nil
+	result, err := i2c.ReadUint16Cmd(command)
+	return int16(result), err
 }
 
 // Perform SMBus Write Word Data transaction.
 func (i2c *I2C) WriteInt16Cmd(command uint8, value int16) error {
 	return i2c.WriteUint16Cmd(command, uint16(value))
-}
-
-// Perform SMBus Quick transaction.
-func (i2c *I2C) WriteQuick() error {
-	result, err := C.i2c_smbus_write_quick(C.int(i2c.file.Fd()), C.I2C_SMBUS_WRITE)
-	if result != 0 {
-		return err
-	}
-	return nil
 }
 
 // Perform SMBus Process Call transaction.
@@ -172,11 +156,11 @@ func (i2c *I2C) WriteQuick() error {
 //
 // Set _compat = False on the SMBus instance to get a return value.
 func (i2c *I2C) ProcessCall(command uint8, value uint16) (uint16, error) {
-	result, err := C.i2c_smbus_process_call(C.int(i2c.file.Fd()), C.__u8(command), C.__u16(value))
-	if result == -1 {
+	_, err := i2c.SmbusAccess(C.I2C_SMBUS_WRITE, command, C.I2C_SMBUS_PROC_CALL, unsafe.Pointer(&value))
+	if err != nil {
 		return 0, err
 	}
-	return uint16(result), nil
+	return 0xFFFF & value, nil
 }
 
 // Perform SMBus Block Process Call transaction.
@@ -188,8 +172,8 @@ func (i2c *I2C) ProcessCallBlock(command uint8, block []byte) ([]byte, error) {
 	data := make([]byte, length+1, C.I2C_SMBUS_BLOCK_MAX+2)
 	data[0] = byte(length)
 	copy(data[1:], block)
-	result, err := C.i2c_smbus_access(C.int(i2c.file.Fd()), C.I2C_SMBUS_WRITE, C.__u8(command), C.I2C_SMBUS_BLOCK_PROC_CALL, (*C.union_i2c_smbus_data)(unsafe.Pointer(&data[0])))
-	if result != 0 {
+	_, err := i2c.SmbusAccess(C.I2C_SMBUS_WRITE, command, C.I2C_SMBUS_BLOCK_PROC_CALL, unsafe.Pointer(&data[0]))
+	if err != nil {
 		return nil, err
 	}
 	return data[1 : 1+data[0]], nil
@@ -198,8 +182,8 @@ func (i2c *I2C) ProcessCallBlock(command uint8, block []byte) ([]byte, error) {
 // Perform SMBus Read Block Data transaction.
 func (i2c *I2C) ReadBlock(command uint8) ([]byte, error) {
 	data := make([]byte, C.I2C_SMBUS_BLOCK_MAX+2)
-	result, err := C.i2c_smbus_access(C.int(i2c.file.Fd()), C.I2C_SMBUS_READ, C.__u8(command), C.I2C_SMBUS_BLOCK_DATA, (*C.union_i2c_smbus_data)(unsafe.Pointer(&data[0])))
-	if result != 0 {
+	_, err := i2c.SmbusAccess(C.I2C_SMBUS_READ, command, C.I2C_SMBUS_BLOCK_DATA, unsafe.Pointer(&data[0]))
+	if err != nil {
 		return nil, err
 	}
 	return data[1 : 1+data[0]], nil
@@ -214,15 +198,12 @@ func (i2c *I2C) WriteBlock(command uint8, block []byte) error {
 	data := make([]byte, length+1)
 	data[0] = byte(length)
 	copy(data[1:], block)
-	result, err := C.i2c_smbus_access(C.int(i2c.file.Fd()), C.I2C_SMBUS_WRITE, C.__u8(command), C.I2C_SMBUS_BLOCK_DATA, (*C.union_i2c_smbus_data)(unsafe.Pointer(&data[0])))
-	if result != 0 {
-		return err
-	}
-	return nil
+	_, err := i2c.SmbusAccess(C.I2C_SMBUS_WRITE, command, C.I2C_SMBUS_BLOCK_DATA, unsafe.Pointer(&data[0]))
+	return err
 }
 
 // TODO: Perform I2C Block Read transaction.
-// With if len == 32 then arg = C.I2C_SMBUS_I2C_BLOCK_BROKEN
+// With if len == 32 then arg = C.I2C_SMBUS_I2C_BLOCK_BROKEN instead of I2C_SMBUS_I2C_BLOCK_DATA ???
 
 func (i2c *I2C) Close() error {
 	return i2c.file.Close()
